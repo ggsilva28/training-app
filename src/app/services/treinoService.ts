5c079faa2a07198662fec0320b88603fd208d9aa
ggsilva <gagsilva28@gmail.com>
2022-01-07 21:25:14 -0300

    Visual Update

--- src/app/services/treinoService.ts
+++ src/app/services/treinoService.ts
@@ -67,6 +67,7 @@ export class TreinosService {
                     cssClass: 'secondary',
                 }, {
                     text: 'Adicionar',
+                    cssClass: 'primary',
                 }
             ]
         });
